b7ad3675e9a46a765385d682e7c10666659e9841
kweku-ander <alexanderdarkoh8@gmail.com>
2025-05-20 01:31:24 +0000

    Revert "project setup"
    This reverts commit a62aa4ff7e60af431f9994d50d651281fe09a6e3.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import inject from '@rollup/plugin-inject' 
 
 export default defineConfig({
-  plugins: [react(), inject({
-      Buffer: ['buffer', 'Buffer'], // Polyfill Buffer
-    })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },  
-  optimizeDeps: {
-    include: ['buffer'], // Ensure buffer is pre-bundled
   },
   base: "./",
 });
