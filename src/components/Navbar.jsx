df18f07ad811c202996e42f2ee05dc89f233f9d7
kweku-ander <alexanderdarkoh8@gmail.com>
2025-06-03 04:14:38 +0000

    commit for navbar mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import CustomButton from './CustomButton';
 import { usePrivy } from '@privy-io/react-auth';
-import { search } from '../assets';
+import { menu, search } from '../assets';
 import { ConsoleLogWriter } from 'drizzle-orm';
+import { IconHeartHandshake } from '@tabler/icons-react';
+import {navLinks} from '../constants'; 
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 const Navbar = () => {
+    const navigate = useNavigate();
     const {ready, authenticated, login, user, logout} = usePrivy();
+    const [toggleDrawer, setToggleDrawer] = useState (false);
+    const [isActive, setIsActive] = useState('dashboard');
     console.log("user information", user)
 
     const handleLogInLogout = useCallback(() => {
@@ -48,8 +57,54 @@ const Navbar = () => {
             handleClick={handleLogInLogout}
             />
             </div>
+
+             
+
+            <div className='relative flex items-center justify-between sm:hidden'>
+                <div className='flex h-[40px] cursor-pointer items-center justify-center
+                rounded-[10px] bg-[#2c2f32]
+                '>
+                   <IconHeartHandshake  size={40} color='#1ec070' className='p-2'/> 
+                </div>
+                <img src={menu} alt="menu" 
+                className='h-[34px] w-[34px] cursor-pointer object-contain'
+                onClick={() => {setToggleDrawer ((prev) => !prev );
+
+                }}
+                />
+
+                <div className= {`absolute left-0 right-0 top-[60px] z-10 bg-[#1c1c24] py-4 shadow-secondary
+                    ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'}
+                    transition-all duration-700 `} 
+                    >
+                        <ul className='mb-4'>
+                            {navLinks.map((link)=>{
+                                return(
+                                <li key={link.name}
+                                className={`flex p-4 ${isActive === link.name && "bg-[#3a3a43]"} cursor-pointer text-[#8a8a8a] gap-4 items-center`}
+                                onClick={
+                                    () =>{
+                                        setIsActive(link.name);
+                                        setToggleDrawer(false);
+                                        navigate(link.link)
+                                    }
+                                }
+                                >
+                                    <img
+                                        src={link.imageUrl}
+                                        alt={link.name}
+                                        className="w-[20px] h-[20px] object-contain"
+                                    />
+                                    {link.name}
+                                </li>
+                                );
+                            })}
+                        </ul>   
+                </div>
+
+            </div>
         </div>
     );
-}
+} 
 
 export default Navbar;
